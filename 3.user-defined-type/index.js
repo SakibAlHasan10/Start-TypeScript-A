cb83e7f8bbd4dff890f8585d1ff40a5529f3656d
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-04 21:44:31 +0600

    added union type

---
+++ 3.user-defined-type/index.js
@@ -0,0 +1,5 @@
+// union type basally uses more then one type
+var userAge;
+userAge = "101";
+userAge = 101;
+userAge = true;
---
+++ 3.user-defined-type/index.ts
@@ -0,0 +1,5 @@
+// union type basally uses more then one type
+let userAge : number | string | boolean;
+userAge= "101";
+userAge = 101;
+userAge = true;
