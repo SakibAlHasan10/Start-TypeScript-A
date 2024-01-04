51d9914ca6925b9613d9f6c6306ccc2268a3a5e9
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-04 22:15:29 +0600

    added tow way use to array

---
+++ 3.user-defined-type/5.2-Array-type/index.js
@@ -0,0 +1,6 @@
+// array type
+// using string[] or Array<string>
+// let userName : string[];
+var userName;
+userName = ["sakil", "samir", "sakib"];
+console.log(userName[0]);
--- 3.user-defined-type/5.2-Array-type/index.ts
+++ 3.user-defined-type/5.2-Array-type/index.ts
@@ -1,2 +1,8 @@
 // array type
+// using string[] or Array<string>
 
+// let userName : string[];
+let userName: Array<string>;
+userName = ["sakil", "samir", "sakib"]
+
+console.log(userName[0])
