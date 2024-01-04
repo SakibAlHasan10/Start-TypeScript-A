9251dc9fe031bc3e35a386edfc883766206285bd
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-04 22:22:14 +0600

    added sort

--- 3.user-defined-type/5.2-Array-type/index.js
+++ 3.user-defined-type/5.2-Array-type/index.js
@@ -4,3 +4,8 @@
 var userName;
 userName = ["sakil", "samir", "sakib"];
 console.log(userName[0]);
+// multiple array type
+var multipleArray;
+multipleArray = [459, "book", 'pean'];
+userName.sort();
+console.log(userName);
--- 3.user-defined-type/5.2-Array-type/index.ts
+++ 3.user-defined-type/5.2-Array-type/index.ts
@@ -6,3 +6,11 @@ let userName: Array<string>;
 userName = ["sakil", "samir", "sakib"]
 
 console.log(userName[0])
+
+// multiple array type
+
+let multipleArray : (string | number)[];
+multipleArray=[459, "book", 'pean']
+
+userName.sort()
+console.log(userName)
