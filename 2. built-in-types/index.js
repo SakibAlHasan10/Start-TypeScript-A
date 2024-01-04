d765ffb4bedcf8ce985dfccf47c466d78630c8ac
SakibAlHasan10 <sakibalhasan.me1@gmail.com>
2024-01-04 21:10:28 +0600

    added js in ts

--- 2. built-in-types/index.js
+++ 2. built-in-types/index.js
@@ -1,4 +1,14 @@
 // built-in data types : number, string, boolean, void, undefined, null
 var userId;
-var userName;
+var firstName;
+var lastName;
+var fullName;
 var isActivated;
+userId = 102;
+firstName = "Sakib";
+lastName = "Al Hasan";
+isActivated = true;
+// concat 
+fullName = firstName.concat(lastName);
+console.log(fullName.split(" "));
+console.log(userId);
--- 2. built-in-types/index.ts
+++ 2. built-in-types/index.ts
@@ -1,6 +1,18 @@
 // built-in data types : number, string, boolean, void, undefined, null
 
 let userId:number;
-let userName :string;
+let firstName :string;
+let lastName : string;
+let fullName : string;
 let isActivated : boolean;
- 
+
+userId = 102;
+firstName = "Sakib";
+lastName = "Al Hasan";
+isActivated = true;
+
+// concat 
+fullName = firstName.concat(lastName);
+
+console.log(fullName.split(" "))
+console.log(userId)
